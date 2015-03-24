3b7389775503702801706e4ad584c4d5a4e08bfb
Craig Hargraves <cs_hargraves@yahoo.com>
2015-03-25 09:37:13 +1000

    Craig's finished Text game!
    It's amazing! You should try it!

--- Assets/Scripts/Craig/CHTextController.cs
+++ Assets/Scripts/Craig/CHTextController.cs
@@ -37,7 +37,20 @@ public class CHTextController : MonoBehaviour {
 					"a broken mirror on the wall, and the cell door is locked from the outside.\n\n" +
 					"Press S to look at the sheets, M to look at the mirror, L to look at the lock.";
 
-		if (Input.GetKeyDown ("s")) 		{myState = States.sheets_0;}
+		if (Input.GetKeyDown ("s")) 		{myState = States.sheets_0;} 
+		else if (Input.GetKeyDown ("l")) 	{myState = States.lock_0;}
+		else if (Input.GetKeyDown ("m")) 	{myState = States.mirror;}
+	}
+
+	void state_cell_mirror(){
+		// Text for standing in the cell after taking the mirror shard.
+		text.text = "So you're still stuck in a dirty, dank cell. And you're still starved of conversation " +
+					"You're pretty sure your plan is still to escape this place. Only now you have a shard of mirror " +
+					"to help you enact your amazing escape plan. \n\nPress S to look at the sheets, L to look at " +
+					"the lock.";
+
+		if (Input.GetKeyDown ("s")) 		{myState = States.sheets_1;} 
+		else if (Input.GetKeyDown ("l")) 	{myState = States.lock_1;}
 	}
 
 	void state_sheets_0(){
@@ -45,14 +58,16 @@ public class CHTextController : MonoBehaviour {
 		text.text = "You've been sweating in these sheets for two weeks now and they still haven't been " +
 					"taken for cleaning. And they started off rather damp and smelly in the first place. You don't " +
 					"see any way to use them.\n\nPress R to return to roaming your cell.";
+
 		if (Input.GetKeyDown ("r")) 		{myState = States.cell;}
 	}
 
 	void state_sheets_1(){
 		// Text for looking at the sheets on the bed post-mirror. Comes from cell_mirror state.
-		text.text = "Nope Using a mirror to look at these festy sheets doesn't help with the " +
+		text.text = "Nope. Using a mirror to look at these festy sheets doesn't help with the " +
 					"smell at all. And you still can't think of a way to use them to get out.\n\nPress " +
 					"R to return to roaming your cell.";
+
 		if (Input.GetKeyDown ("r")) 		{myState = States.cell_mirror;}
 	}
 
@@ -60,10 +75,41 @@ public class CHTextController : MonoBehaviour {
 		// Text for looking at the broken mirror. Comes from the cell state and returns to the cell_mirror state
 		// if the mirror is taken.
 		text.text = "This mirror was totally broken before you got here. It definitely wasn't your fault! Although " +
-					"now that you at yourself in the mirror, your reflection fractured like your soul it occurs " +
-					" to you that it could be useful to grab a shard.\n\nPress T to take a mirror shard. Press R to return to roaming your cell.";
+					"now that you at yourself in the mirror, your reflection fractured like your soul, it occurs " +
+					" to you that it could be useful to grab a shard.\n\nPress T to take a mirror shard. Press R " +
+					"to return to roaming your cell.";
+
 		if (Input.GetKeyDown ("r")) 			{myState = States.cell;}
 		else if (Input.GetKeyDown ("t")) 		{myState = States.cell_mirror;}
 	}
 
+	void state_lock_0(){
+		// Text for looking at the door's lock without the mirror. Comes from the cell state.
+		text.text = "So it looks like some kind of push-button combination look. Perhaps if you had some way to " +
+					"look at the keys via some kind of reflection, maybe you could see which keys numbers have " +
+					"worn off from overuse . . . If only . . .\n\nPress R to return to perusing your cell.";
+
+		if (Input.GetKeyDown ("r")) 			{myState = States.cell;}
+	}
+
+	void state_lock_1(){
+		// Text for looking at the door's lock with the mirror. Comes from the cell_mirror state. Leads to Freedom state.
+		text.text = "Huh. It seems like this shard of mirror could be used to look at the keypad outside the door. " +
+					"And what do you know? The only key that's worn out is the 9. I guess there can't be too many " +
+					"4 to 6 digit combinations using only the number 9 . . .\n\nPress R to return to perusing your " +
+					"cell because you just don't get it.\nPress U to try unlock the cell by repeatedly pressing 9.";
+
+		if (Input.GetKeyDown ("r")) 			{myState = States.cell_mirror;}
+		else if (Input.GetKeyDown ("u")) 		{myState = States.freedom;}
+	}
+
+	void state_freedom(){
+		// Text for unlocking the door and escaping your cell. Comes from the lock_1 state.
+		text.text = "Holy cow! It was an eight-digit combination! That could have been impossible to crack. Luckily " +
+			"they left it as the manufacturer's default of '99999999'. And now you're out and free to run " +
+			"and skip in the sunshine. Congratulations!\n\nPress R to drop the mirror, walk back into your " +
+			"cell (pulling the door closed behind you), and starting all over again.";
+
+		if (Input.GetKeyDown ("r")) 			{myState = States.cell;}
+	}
 }
